709179bab9c5bb0a2ddd8abdf2aaf30d629bf27b
Evgenia Polozova <eugpoloz@Evgenias-MacBook-Pro.local>
2017-10-22 18:43:17 +0300

    refactor and style <Search/> and the whole /jobs page

--- client/src/components/pages/ExploreJobs/ExploreJobs.js
+++ client/src/components/pages/ExploreJobs/ExploreJobs.js
@@ -1,16 +1,15 @@
 import * as React from "react";
-import styled from "styled-components";
-
 import Search from "./Search";
 
-export const Heading = styled.h2`
-  padding: 0.5em 0;
-  font-weight: 700;
-  font-size: 36px;
-`;
+import { H1 } from "./style";
 
 function ExploreJobs(props) {
-  return [<Heading key={0}>Find your next job</Heading>, <Search key={1} />];
+  return (
+    <div className="container">
+      <H1>Find your next job</H1>
+      <Search />
+    </div>
+  );
 }
 
 export default ExploreJobs;
--- client/src/components/pages/ExploreJobs/Search.js
+++ client/src/components/pages/ExploreJobs/Search.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Container } from "./style";
 
 class Search extends React.Component {
   state = {
@@ -6,19 +7,45 @@ class Search extends React.Component {
   };
 
   handleChange = e => {
-    console.log(e.currentTarget.value);
+    this.setState({
+      // currentTarget in React is better than target
+      fieldInput: e.currentTarget.value
+    });
+  };
+
+  handleSearchClick = () => {
+    console.log(`search for "${this.state.fieldInput}"`);
   };
 
   render() {
+    const placeholder = "Search by job, title, or location...";
+
     return (
-      <input
-        type="search"
-        id="explore-jobs-search"
-        name="explore-jobs-search"
-        placeholder="Search by job, title, or location..."
-        value={this.state.fieldInput}
-        onChange={this.handleChange}
-      />
+      <Container className="row justify-content-center">
+        <div className="col-8">
+          <div className="form-group input-group input-group-lg">
+            <input
+              type="search"
+              id="explore-jobs-search"
+              name="explore-jobs-search"
+              className="form-control form-control-lg"
+              placeholder={placeholder}
+              aria-label={placeholder}
+              value={this.state.fieldInput}
+              onChange={this.handleChange}
+            />
+            <span className="input-group-btn">
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={this.handleSearchClick}
+              >
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </Container>
     );
   }
 }
