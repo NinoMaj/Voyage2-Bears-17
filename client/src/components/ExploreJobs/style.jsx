15db5353408939b34f2c01cb9322cc63405b4e74
Evgenia Polozova <eugpoloz@Evgenias-MacBook-Pro.local>
2017-10-22 16:38:28 +0300

    add page heading

---
+++ client/src/components/ExploreJobs/style.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Heading = styled.h2`
+  padding: 0.5em 0;
+  font-weight: 700;
+  font-size: 36px;
+`;
--- client/src/components/pages/ExploreJobs.js
+++ client/src/components/pages/ExploreJobs.js
@@ -1,7 +1,8 @@
 import * as React from "react";
+import { Heading } from "../ExploreJobs/style";
 
 function ExploreJobs(props) {
-  return <div>explore jobs page</div>;
+  return [<Heading key={0}>Find your next job</Heading>];
 }
 
 export default ExploreJobs;
